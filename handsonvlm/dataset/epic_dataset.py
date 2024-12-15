9ed1607f2c9b571e8e465ef44992073aca583612
chenbao <im.b.c@live.com>
2024-12-16 00:41:16 +0800

    add requirements

--- handsonvlm/dataset/epic_dataset.py
+++ handsonvlm/dataset/epic_dataset.py
@@ -148,11 +148,6 @@ class EpicReasoningConversationDataset(EpicConversationDataset):
         for index, action in tqdm(enumerate(self.epic_hoi_dataset.actions)):
             hoi_feature_dict = self.epic_hoi_dataset.load_hoi_features(action)
             last_image_path = hoi_feature_dict['image_paths'][-1]
-            print("self.reasoning_templates = ", self.reasoning_templates)
-
-            print("last_image_path = ", last_image_path)
-            print("in ", last_image_path in self.reasoning_templates)
-            exit()
             if last_image_path in self.reasoning_templates:
                 questions = self.reasoning_templates[last_image_path]
                 if len(questions) != 0:
